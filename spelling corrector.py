36c22639cf684b8a41a8b03248bd18e7e46df35f
ShivamSharmaDeveloper <shivsharma7821@gmail.com>
2021-05-27 21:41:57 +0530

    Updated

--- spelling corrector.py
+++ spelling corrector.py
@@ -1,6 +1,6 @@
 from textblob import TextBlob
 
-file1=open("input.text", "r+") # write your file name here For example- demo.txt
+file1=open("input.txt", "r+") # write your file name here For example- demo.txt
 a=file1.read()
 
 print("Original Text: "+str(a))
@@ -9,7 +9,7 @@ b=TextBlob(a)
 
 print("Corrected Text: "+str(b.correct()))
 
-d=open("demo.text", "w") # write your file name here For example- corrected.txt
+d=open("demo.txt", "w") # write your file name here For example- corrected.txt
 d.write(str(b.correct()))
 d.close()
 
